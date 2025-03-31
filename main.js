21db333b3f9038a7100b8d667dd0c83876b00d09
carolzitaa <146238302+carolzinha022@users.noreply.github.com>
2025-03-31 11:40:35 -0300

    .

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0) {
         return [dias, horas, minutos, segundos];
     } else {
-        return [5, 12, 30, 12];
+        return [0, 0, 0, 0];
     }
 }
 
